1035219f0c055ab682a79b4217b065a72c4cd9be
dzarudni <dzarudni@cisco.com>
2016-08-25 18:23:31 +0300

    directory walkthrough 1

--- c76cli_sh_mod.py
+++ c76cli_sh_mod.py
@@ -16,28 +16,39 @@ import datetime
 import json
 import gzip
 import shutil
-import xlwt
+
 
 # DEFAULTS
-inDir = '../RawInv50-cli'
+
 
 # DATETIME
 start_time = time.time()
 now = datetime.datetime.now()
 nowDate = now.strftime("%Y%m%d")
 
-inFile = open(inFileName)
 data = {}
 
 inDir = '/Users/dzarudni/Documents/csco/CUST/mgts/tasks/2016-08-15 -- Migration76-ASR9k/RawInv50-cli'
 
 for inSubDir in os.walk(inDir):
-    path, file = os.path.split(inSubDir[0])
+    pathFull = inSubDir[0]
+    path, file = os.path.split(pathFull)
     file = os.path.splitext(file)[0]
-    print 'Dir: ' + file
-    for inFile in inSubDir[2]:
-        print ' File:' + inFile
-
+    node = file
+    data[node] = {}
+    fileNameTypeSuffix = "show_version.txt"
+    for inFileName in inSubDir[2]:
+        if inFileName.endswith(fileNameTypeSuffix):
+            inFile = open(pathFull + "/" + inFileName)
+            with inFile as inData:
+                for rawline in inData:
+                    line = rawline.strip()
+                    if line.startswith("Cisco IOS Software"):
+                        m = re.search('.* Version (\d\d\..*)\, .*', line)
+                        if m:
+                            version = m.group(1)
+                            data[node].update({'VERSION' : version})
+print data
 
 
 
